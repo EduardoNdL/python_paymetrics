e895a543c3cffe929a66daf27eae85dd5fec86e8
EduardoNdL <eduardo.lima@sptech.school>
2025-09-08 09:40:02 -0300

    add style

--- script.py
+++ script.py
@@ -2,14 +2,29 @@ import psutil, datetime, time, pandas as pd, os, platform
 
 while True:
 
+    print(r"""  ____            _                         _      
+ / ___|__ _ _ __ | |_ _   _ _ __ __ _    __| | ___ 
+| |   / _` | '_ \| __| | | | '__/ _` |  / _` |/ _ \
+| |__| (_| | |_) | |_| |_| | | | (_| | | (_| |  __/
+ \____\__,_| .__/_\__|\__,_|_|  \__,_|  \__,_|\___|
+  __| | __ |_|__| | ___  ___                       
+ / _` |/ _` |/ _` |/ _ \/ __|                      
+| (_| | (_| | (_| | (_) \__ \                      
+ \__,_|\__,_|\__,_|\___/|___/                      """)
+
     disco_usado = ((psutil.disk_io_counters().write_bytes) / 1024 / 1024)
+    print("\n █▒▒▒▒▒▒▒▒▒10%")
     time.sleep(5)
     disco_usadoN = ((psutil.disk_io_counters().write_bytes) / 1024 / 1024)
     taxaI = disco_usadoN - disco_usado
-
+    
+    print("\n ████▒▒▒▒▒▒30%")
+    
     rede_antiga = (psutil.net_io_counters())
+    print("\n █████▒▒▒▒▒50%")
     time.sleep(5)
     rede_nova = psutil.net_io_counters()
+    print("\n ████████▒▒80%")
 
     bytes_enviados = ((rede_nova.bytes_sent - rede_antiga.bytes_sent) / 1024 / 1024 )
     bytes_recebidos = ((rede_nova.bytes_recv - rede_antiga.bytes_recv) / 1024 / 1024 )
@@ -24,6 +39,8 @@ while True:
             else:
                 if itens.startswith(("eth", "enp", "ens")) and mac_Ethernet == None:
                     mac_Ethernet = especicacoes.address
+                    
+    print("\n ██████████100%")
     
     timestamp = time.time()
     data_formatada = datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
@@ -40,6 +57,8 @@ while True:
         "Mb_Enviados": [bytes_enviados],
         "Mb_Recebidos": [bytes_recebidos]
     }
+    
+    print("\n ⋘ creating data... ⋙")
     
     df = pd.DataFrame(valores)
     if os.path.exists("DadosHardware.csv"):
